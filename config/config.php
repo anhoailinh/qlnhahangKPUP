0b5b4a24f2aae0ccebb3d604f88b606fc8ab2f85
anhoailinh <155376800+anhoailinh@users.noreply.github.com>
2025-05-24 23:56:25 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
-define("DB_HOST", "localhost");
-define("DB_USER", "root");
-define("DB_PASS", "");
-define("DB_NAME", "gs_restaurant");
+define("DB_HOST", "sql309.infinityfree.com");
+define("DB_USER", "if0_39066208");
+define("DB_PASS", "Linh3011");
+define("DB_NAME", "if0_39066208_gs_restaurant");
 ?>
