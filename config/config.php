d132d7a8fe0bcde838f2913b8b415294d25da164
anhoailinh <155376800+anhoailinh@users.noreply.github.com>
2025-05-25 11:03:14 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-
 define("DB_HOST", "nhahangkpup.mysql.database.azure.com");
-define("DB_USER", "nhahangkpup");
+define("DB_USER", "nhahangkpup@nhahangkpup"); // Thường user ở Azure phải có dạng username@hostname
 define("DB_PASS", "Linh3011");
 define("DB_NAME", "gs_restaurant");
+define("DB_PORT", 3306);
 ?>
