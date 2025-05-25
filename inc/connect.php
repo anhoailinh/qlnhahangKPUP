513a16e31a4b77e3f05c70cb52930f2961531987
anhoailinh <155376800+anhoailinh@users.noreply.github.com>
2025-05-25 10:52:00 +0700

    Update connect.php

--- inc/connect.php
+++ inc/connect.php
@@ -1,13 +1,26 @@
-<?php 
-    $server = 'nhahangkpup.mysql.database.azure.com';
-    $user = 'nhahangkpup';
-    $pass = 'Linh3011';
-    $database ='gs_restaurant';
+<?php
+$server = 'nhahangkpup.mysql.database.azure.com';
+$port = 3306;
+$user = 'nhahangkpup@nhahangkpup'; // username theo dạng username@hostname trên Azure
+$pass = 'Linh3011';
+$database = 'gs_restaurant';
 
-    $conn= new mysqli($server,$user,$pass,$database);
-    if($conn){
-        mysqli_query($conn, " SET NAMES 'utf8' ");
-    }else{
-        echo "ket noi khong thnah coong";
-    }
+// Khởi tạo mysqli
+$conn = mysqli_init();
+if (!$conn) {
+    die('mysqli_init failed');
+}
 
+// Bật SSL (không dùng file chứng chỉ, vì ssl-mode=require là bắt buộc SSL nhưng Azure cho phép kết nối không cần client cert)
+$conn->ssl_set(NULL, NULL, NULL, NULL, NULL);
+
+// Kết nối với flag MYSQLI_CLIENT_SSL
+if (!$conn->real_connect($server, $user, $pass, $database, $port, NULL, MYSQLI_CLIENT_SSL)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+
+// Thiết lập charset UTF-8
+mysqli_query($conn, "SET NAMES 'utf8'");
+
+echo "Kết nối thành công!";
+?>
